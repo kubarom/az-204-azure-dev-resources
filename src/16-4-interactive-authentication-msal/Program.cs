95cc6975bb0f1def151b1a0093aef79efeac3f49
Romaniec Jakub <jakub.romaniec@outlook.com>
2024-06-13 13:16:18 +0200

    interactive auth with MSAL lab

---
+++ src/16-4-interactive-authentication-msal/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//https://portal.azure.com/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/RegisteredApps
+const string _clientId = ""; //Registered application Id
+const string _tenantId = ""; // App's Tenant id
+
+var app = PublicClientApplicationBuilder
+    .Create(_clientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+    .WithRedirectUri("http://localhost")
+    .Build();
+        
+string[] scopes = { "user.read" };
+
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+Console.WriteLine($"Token:\t{result.AccessToken}");
